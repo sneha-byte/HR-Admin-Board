bfac719f979b3db523d646bcbd302c1361d337a1
Sneha Sadhwani 82959017 <snehasadhwani@hotmail.com>
2025-08-07 14:44:47 -0700

    initial work

---
+++ src/menuItems.jsx
@@ -0,0 +1,14 @@
+import { LayoutDashboard, UserStar, CalendarRange, Users, CircleUserRound, Headset, Settings } from "lucide-react";
+
+// Array of menu items with icons and tags
+const menuItems = [
+    { image: <LayoutDashboard size={18} />, tag: "Dashboard" },
+    { image: <UserStar size={18} />, tag: "Recruitment" },
+    { image: <CalendarRange size={18} />, tag: "Schedule" },
+    { image: <Users size={18} />, tag: "Employee" },
+    { image: <CircleUserRound size={18} />, tag: "Department" },
+    { image: <Headset size={18} />, tag: "Support" },
+    { image: <Settings size={18} />, tag: "Settings" },
+];
+
+export default menuItems;
